58069e4eb38bab51c2398ee69b0529baaf3f6254
bethurban <beth.urban@gmail.com>
2019-08-16 12:12:53 -0400

    Fix blog_spec association test to look for :posts. Add association and validations to Blog model. Add validations to Post model.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,4 @@
 class Post < ApplicationRecord
   belongs_to :blog
+  validates_presence_of :title, :content
 end
--- spec/models/blog_spec.rb
+++ spec/models/blog_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Blog, type: :model do
   # Association test - make sure Blog has a 1:m relationship with Post
-  it { should have_many(:items).dependent(:destroy) }
+  it { should have_many(:posts).dependent(:destroy) }
 
   #Validation tests - make sure title and created_by columns are present before saving
   it { should validate_presence_of(:title) }
