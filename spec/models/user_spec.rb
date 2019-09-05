506e40d8dcda8f23b9e425cd8836cc98c1e3d0fe
bethurban <beth.urban@gmail.com>
2019-09-05 08:48:49 -0400

    Define User model spec - association test and validation tests.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  #Association test - make sure User has a 1:m relationship with Blog
+  it { should have_many(:todos) }
+
+  #Validation tests - make sure name, email, and password_digest are present
+  #before saving
+  it { should validate_presence_of(:name) }
+  it { should validate_presence_of(:email) }
+  it { should validate_presence_of(:password_digest) }
+  
 end
