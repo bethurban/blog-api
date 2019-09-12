3e079a8128e3a49ea1d06a9e0456cec44ed10e3d
bethurban <beth.urban@gmail.com>
2019-09-12 16:22:15 -0400

    Update rails helper to include all types of RequestSpecHelper and ControllerSpecHelper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -73,7 +73,8 @@ RSpec.configure do |config|
   config.include FactoryBot::Syntax::Methods
 
   #Include support directory for all request specs
-  config.include RequestSpecHelper, type: :request
+  config.include RequestSpecHelper
+  config.include ControllerSpecHelper
 
   # start by truncating all the tables but then use the faster transaction strategy the rest of the time.
   config.before(:suite) do
